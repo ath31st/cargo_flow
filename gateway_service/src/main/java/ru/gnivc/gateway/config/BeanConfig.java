514be3c322649fc2a5a3454d56d28c930d2334c1
ath31st <ath31st@yandex.ru>
2024-05-21 14:23:00 +0300

    add BeanConfig with bean keycloak

---
+++ gateway_service/src/main/java/ru/gnivc/gateway/config/BeanConfig.java
@@ -0,0 +1,38 @@
+package ru.gnivc.gateway.config;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+  @Value("${keycloak.auth-server-url}")
+  private String serverUrl;
+  @Value("${keycloak.realm}")
+  private String realm;
+  @Value("${keycloak.password}")
+  private String password;
+  @Value("${keycloak.username}")
+  private String username;
+
+  @Bean
+  public Keycloak keycloak() {
+    return KeycloakBuilder.builder()
+        .serverUrl(serverUrl)
+        .realm(realm)
+        .grantType(OAuth2Constants.PASSWORD)
+        .username(username)
+        .password(password)
+        .clientId("gateway-service")
+        .build();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
