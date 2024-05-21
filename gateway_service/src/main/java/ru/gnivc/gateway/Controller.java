41dc982cffc4652d2866980b19c8d2123da0d3a1
ath31st <ath31st@yandex.ru>
2024-05-21 21:41:55 +0300

    rework Controller, add UserRequestDto

--- gateway_service/src/main/java/ru/gnivc/gateway/Controller.java
+++ gateway_service/src/main/java/ru/gnivc/gateway/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-  @GetMapping("/")
+  @GetMapping("/test")
   public String home(Principal principal) {
     return "Hello, " + principal.getName();
   }
