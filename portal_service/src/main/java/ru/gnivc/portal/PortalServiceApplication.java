6cdea7ddc510efc615e14354fb2badeec1144df0
ath31st <ath31st@yandex.ru>
2024-05-23 18:03:28 +0300

    initialization portal service

---
+++ portal_service/src/main/java/ru/gnivc/portal/PortalServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gnivc.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PortalServiceApplication.class, args);
+  }
+
+}
---
+++ portal_service/src/test/java/ru/gnivc/portal/PortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gnivc.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
