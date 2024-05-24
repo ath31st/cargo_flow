4ae594b3fbff2406bc75ec358d64c41766fbbef2
ath31st <ath31st@yandex.ru>
2024-05-24 19:18:14 +0300

    add stub logist service

---
+++ logist_service/src/main/java/ru/gnivc/logist/LogistServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gnivc.logist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogistServiceApplication.class, args);
+  }
+
+}
