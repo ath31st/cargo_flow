f598635898348b0e8875373547b49ea6d6a5eee9
ath31st <ath31st@yandex.ru>
2024-05-24 19:22:15 +0300

    add stub driver service

---
+++ driver_service/src/main/java/ru/gnivc/driver/DriverServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gnivc.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DriverServiceApplication.class, args);
+  }
+
+}
