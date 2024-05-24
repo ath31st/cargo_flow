637eb8124aa597906efe5011a5ae56568760b859
ath31st <ath31st@yandex.ru>
2024-05-24 19:25:06 +0300

    add stub dwh service

---
+++ dwh_service/src/main/java/ru/gnivc/dwh/DwhServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gnivc.dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DwhServiceApplication.class, args);
+  }
+
+}
