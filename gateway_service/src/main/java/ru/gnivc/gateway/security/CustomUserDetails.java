b9b9e025920fd065a2c2b4817e0ff7953b72f6c0
ath31st <ath31st@yandex.ru>
2024-05-24 12:54:52 +0300

    add CustomUserDetails class

---
+++ gateway_service/src/main/java/ru/gnivc/gateway/security/CustomUserDetails.java
@@ -0,0 +1,48 @@
+package ru.gnivc.gateway.security;
+
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+  private final String username;
+  private final String password;
+  private final Collection<GrantedAuthority> roles;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+}
