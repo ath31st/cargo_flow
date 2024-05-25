ca05fe4a80832c40ca4d4b8da8fea1cc63dbee8a
ath31st <ath31st@yandex.ru>
2024-05-25 10:58:03 +0300

    portal || add AbstractException and child KeycloakServiceException

---
+++ portal_service/src/main/java/ru/gnivc/portal/exception/AbstractException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Vladimir Olennikov.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.gnivc.portal.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class AbstractException extends RuntimeException {
+  private final HttpStatus status;
+
+  protected AbstractException(HttpStatus status, String errorMessage) {
+    super(errorMessage);
+    this.status = status;
+  }
+}
