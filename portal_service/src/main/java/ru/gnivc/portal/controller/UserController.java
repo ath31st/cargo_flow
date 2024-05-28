059058e8bdd7aeb27a3b09dbe3c9f8a7798d9c90
ath31st <ath31st@yandex.ru>
2024-05-28 15:37:42 +0300

    portal || change http methods for user controller

--- portal_service/src/main/java/ru/gnivc/portal/controller/UserController.java
+++ portal_service/src/main/java/ru/gnivc/portal/controller/UserController.java
@@ -6,10 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gnivc.portal.dto.user.IndividualRegisterReq;
 import ru.gnivc.portal.dto.user.NewUserDataReq;
@@ -34,20 +35,20 @@ public class UserController {
     return new ResponseEntity<>(tempPassword, HttpStatus.CREATED);
   }
 
-  @PostMapping("/reset-password")
-  public ResponseEntity<String> resetPassword(Principal principal, @RequestParam String email) {
+  @GetMapping("/reset-password/{email}")
+  public ResponseEntity<String> resetPassword(Principal principal, @PathVariable String email) {
     final String password = userService.changePassword(principal, email, null);
     return ResponseEntity.ok(password);
   }
 
-  @PostMapping("/change-password")
+  @PutMapping("/change-password")
   public ResponseEntity<HttpStatus> changePassword(
       Principal principal, @RequestBody NewUserPasswordReq req) {
     userService.changePassword(principal, req.email(), req.newPassword());
     return ResponseEntity.ok().build();
   }
 
-  @PostMapping("/change-data")
+  @PutMapping("/change-data")
   public ResponseEntity<HttpStatus> changeUserData(
       Principal principal, @RequestBody NewUserDataReq req) {
     userService.changeData(principal, req);
