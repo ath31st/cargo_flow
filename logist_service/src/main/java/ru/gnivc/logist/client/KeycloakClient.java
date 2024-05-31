16ca4ace2cec522f7960a8aeddc7d7902ced25a5
ath31st <ath31st@yandex.ru>
2024-05-31 14:31:50 +0300

    logist || add methods for processing drivers

--- logist_service/src/main/java/ru/gnivc/logist/client/KeycloakClient.java
+++ logist_service/src/main/java/ru/gnivc/logist/client/KeycloakClient.java
@@ -1,6 +1,8 @@
 package ru.gnivc.logist.client;
 
 import jakarta.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.UserResource;
@@ -9,7 +11,8 @@ import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import ru.gnivc.common.exception.UserServiceException;
+import ru.gnivc.common.exception.KeycloakClientException;
+import ru.gnivc.common.role.KeycloakRealmRoles;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +27,28 @@ public class KeycloakClient {
     return user.getFirstName() + " " + user.getLastName();
   }
 
+  public void validateDriverInCompany(String id, String companyId) {
+    UserRepresentation user = getUserRepById(id);
+    if (!hasRoleInCompany(user, KeycloakRealmRoles.DRIVER.getAttributeName(), companyId)) {
+      throw new KeycloakClientException(HttpStatus.NOT_FOUND,
+          String.format("Driver with id: %s in company: %s not found", id, companyId));
+    }
+  }
+
+  private boolean hasRoleInCompany(UserRepresentation user, String roleName, String companyId) {
+    Map<String, List<String>> attributes = user.getAttributes();
+    if (attributes != null && attributes.containsKey(roleName)) {
+      return attributes.get(roleName).contains(companyId);
+    }
+    return false;
+  }
+
   private UserRepresentation getUserRepById(String id) {
     UserResource userResource = getUsersResource().get(id);
     try {
       return userResource.toRepresentation();
     } catch (NotFoundException e) {
-      throw new UserServiceException(HttpStatus.NOT_FOUND, "User with id: " + id + " not found");
+      throw new KeycloakClientException(HttpStatus.NOT_FOUND, "User with id: " + id + " not found");
     }
   }
 
