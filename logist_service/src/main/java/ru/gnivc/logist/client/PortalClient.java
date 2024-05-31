31020f688c54a8ae9da278dd88791e7431b2520b
ath31st <ath31st@yandex.ru>
2024-05-31 18:12:14 +0300

    logist || add getting vehicle license plate for portal

--- logist_service/src/main/java/ru/gnivc/logist/client/PortalClient.java
+++ logist_service/src/main/java/ru/gnivc/logist/client/PortalClient.java
@@ -7,6 +7,11 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @RequiredArgsConstructor
 public class PortalClient {
+  private final String PORTAL_PATH = "http://portal/portal/v1/companies";
   private final RestTemplate restTemplate;
 
+  public String getCompanyVehicleLicensePlate(int companyId, int vehicleId) {
+    String url = String.format("%s/%d/vehicles/%d", PORTAL_PATH, companyId, vehicleId);
+    return restTemplate.getForObject(url, String.class);
+  }
 }
