72de55c51711e90cff05ad70725e36fa177040f2
ath31st <ath31st@yandex.ru>
2024-05-31 11:17:20 +0300

    Revert "logist || sql scripts (tasks, task routes, route events, route locations) for flyway migration"
    This reverts commit 18e4ec41a6b55b1dd59514a7619d6a256f11375f.

--- portal_service/src/main/resources/db/migration/create_tables/V008__create_task_routes_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE task_routes
-(
-    route_id    SERIAL PRIMARY KEY,
-    task_id    INT NOT NULL REFERENCES tasks (task_id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    start_time TIMESTAMP,
-    end_time   TIMESTAMP
-);
--- portal_service/src/main/resources/db/migration/create_tables/V010__create_route_locations_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE route_locations
-(
-    location_id SERIAL PRIMARY KEY,
-    route_id    INT           NOT NULL REFERENCES task_routes (route_id),
-    latitude    DECIMAL(9, 6) NOT NULL,
-    longitude   DECIMAL(9, 6) NOT NULL,
-    recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
