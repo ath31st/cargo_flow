93db4a2e3bdee3e4e3a2b53c1ff73c13186ebf92
ath31st <ath31st@yandex.ru>
2024-06-01 00:02:39 +0300

    logist || add methods in task controller and service

--- logist_service/src/main/java/ru/gnivc/logist/controller/TaskController.java
+++ logist_service/src/main/java/ru/gnivc/logist/controller/TaskController.java
@@ -1,12 +1,33 @@
 package ru.gnivc.logist.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.gnivc.logist.dto.NewTaskReq;
+import ru.gnivc.logist.service.TaskService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/logist/v1")
 public class TaskController {
+  private final TaskService taskService;
 
+  @GetMapping("/tasks/{companyId}/{taskId}")
+  public ResponseEntity<String> getTask(@PathVariable int companyId,
+                                        @PathVariable int taskId) {
+    return ResponseEntity.ok().body(taskService.getTask(companyId, taskId));
+  }
+
+  @PostMapping("/tasks/{companyId}/create-task")
+  public ResponseEntity<HttpStatus> createTask(
+      @PathVariable int companyId, @RequestBody NewTaskReq req) {
+    taskService.createTask(companyId, req);
+    return ResponseEntity.ok(HttpStatus.CREATED);
+  }
 }
--- logist_service/src/main/java/ru/gnivc/logist/service/TaskService.java
+++ logist_service/src/main/java/ru/gnivc/logist/service/TaskService.java
@@ -2,11 +2,31 @@ package ru.gnivc.logist.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.gnivc.common.client.PortalClient;
+import ru.gnivc.logist.dto.NewTaskReq;
+import ru.gnivc.logist.entity.Task;
 import ru.gnivc.logist.repository.TaskRepository;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
   private final TaskRepository taskRepository;
+  private final PortalClient portalClient;
 
+  public String getTask(int companyId, int taskId) {
+    return "";
+  }
+
+  public void createTask(int companyId, NewTaskReq newTask) {
+
+    Task t = new Task();
+    t.setStartPoint(newTask.startPoint());
+    t.setEndPoint(newTask.endPoint());
+    t.setDriverKeycloakId(newTask.driverKeycloakId());
+    t.setCargoDescription(newTask.cargoDescription());
+    t.setCompanyVehicleId(newTask.companyVehicleId());
+    t.setCompanyId(companyId);
+
+    taskRepository.save(t);
+  }
 }
