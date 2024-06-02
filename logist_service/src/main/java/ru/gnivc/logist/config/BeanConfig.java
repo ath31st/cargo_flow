2456f73203a4b3f3909499889787e01956748dd3
ath31st <ath31st@yandex.ru>
2024-06-02 14:35:47 +0300

    logist || add cargo flow starter

--- logist_service/src/main/java/ru/gnivc/logist/config/BeanConfig.java
+++ logist_service/src/main/java/ru/gnivc/logist/config/BeanConfig.java
@@ -1,19 +1,15 @@
 package ru.gnivc.logist.config;
 
-import java.util.Collections;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import ru.gnivc.common.client.PortalClient;
-import ru.gnivc.common.interceptor.TokenInterceptor;
-import ru.gnivc.common.validator.PermissionValidator;
 
 @Configuration
+@ComponentScan("ru.gnivc.starter")
 public class BeanConfig {
   @Value("${keycloak.auth-server-url}")
   private String serverUrl;
@@ -39,23 +35,5 @@ public class BeanConfig {
         .clientId(clientId)
         .clientSecret(secret)
         .build();
-  }
-
-  @Bean
-  @LoadBalanced
-  public RestTemplate restTemplate() {
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.setInterceptors(Collections.singletonList(new TokenInterceptor()));
-    return restTemplate;
-  }
-
-  @Bean
-  public PermissionValidator permissionValidator() {
-    return new PermissionValidator();
-  }
-
-  @Bean
-  public PortalClient portalClient() {
-    return new PortalClient(restTemplate());
   }
 }
