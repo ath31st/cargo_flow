f228880fbcfdad6fe6ae5730a6c2c60622e7a6ae
ath31st <ath31st@yandex.ru>
2024-06-02 18:51:12 +0300

    starter || add TaskService logic get task by id and company id, update controller

--- logist_service/src/main/java/ru/gnivc/logist/controller/TaskController.java
+++ logist_service/src/main/java/ru/gnivc/logist/controller/TaskController.java
@@ -3,6 +3,7 @@ package ru.gnivc.logist.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gnivc.logist.dto.NewTaskReq;
+import ru.gnivc.logist.dto.TaskDto;
 import ru.gnivc.logist.service.TaskService;
 
 @RestController
@@ -18,12 +20,14 @@ import ru.gnivc.logist.service.TaskService;
 public class TaskController {
   private final TaskService taskService;
 
+  @PreAuthorize("#permissionValidator.hasCompanyLogistAccess(#companyId)")
   @GetMapping("/tasks/{companyId}/{taskId}")
-  public ResponseEntity<String> getTask(@PathVariable int companyId,
-                                        @PathVariable int taskId) {
-    return ResponseEntity.ok().body(taskService.getTask(companyId, taskId));
+  public ResponseEntity<TaskDto> getTask(@PathVariable int companyId,
+                                         @PathVariable int taskId) {
+    return ResponseEntity.ok().body(taskService.getTaskDto(companyId, taskId));
   }
 
+  @PreAuthorize("#permissionValidator.hasCompanyLogistAccess(#companyId)")
   @PostMapping("/tasks/{companyId}/create-task")
   public ResponseEntity<HttpStatus> createTask(
       @PathVariable int companyId, @RequestBody NewTaskReq req) {
--- logist_service/src/main/java/ru/gnivc/logist/service/TaskService.java
+++ logist_service/src/main/java/ru/gnivc/logist/service/TaskService.java
@@ -1,9 +1,13 @@
 package ru.gnivc.logist.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.gnivc.common.client.PortalClient;
+import ru.gnivc.common.exception.TaskServiceException;
 import ru.gnivc.logist.dto.NewTaskReq;
+import ru.gnivc.logist.dto.TaskDto;
 import ru.gnivc.logist.entity.Task;
 import ru.gnivc.logist.repository.TaskRepository;
 
@@ -13,9 +17,6 @@ public class TaskService {
   private final TaskRepository taskRepository;
   private final PortalClient portalClient;
 
-  public String getTask(int companyId, int taskId) {
-    return "";
-  }
 
   public void createTask(int companyId, NewTaskReq newTask) {
 
@@ -28,5 +29,29 @@ public class TaskService {
     t.setCompanyId(companyId);
 
     taskRepository.save(t);
+  }
+
+  public TaskDto getTaskDto(int companyId, int taskId) {
+    Task t = getTask(companyId, taskId);
+
+    String driverFullName =
+        portalClient.getCompanyDriverFullName(companyId, t.getDriverKeycloakId());
+    String vehicleLicensePlate =
+        portalClient.getCompanyVehicleLicensePlate(companyId, t.getCompanyVehicleId());
+
+    return TaskDto.builder()
+        .id(t.getId())
+        .startPoint(t.getStartPoint())
+        .endPoint(t.getEndPoint())
+        .driverFullName(driverFullName)
+        .cargoDescription(t.getCargoDescription())
+        .companyVehicleLicensePlate(vehicleLicensePlate)
+        .build();
+  }
+
+  private Task getTask(int companyId, int taskId) {
+    final Optional<Task> optionalTask = taskRepository.findByCompanyIdAndId(companyId, taskId);
+    return optionalTask.orElseThrow(() -> new TaskServiceException(HttpStatus.NOT_FOUND,
+        "Task with id: " + taskId + " not found"));
   }
 }
