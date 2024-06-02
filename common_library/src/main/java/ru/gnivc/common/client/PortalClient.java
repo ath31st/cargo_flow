75cec5b44b1714dd99278ae8d6f481bf085a8cd6
ath31st <ath31st@yandex.ru>
2024-06-02 12:14:41 +0300

    portal || add methods for getting full name and validating a driver

--- common_library/src/main/java/ru/gnivc/common/client/PortalClient.java
+++ common_library/src/main/java/ru/gnivc/common/client/PortalClient.java
@@ -9,7 +9,17 @@ public class PortalClient {
   private final RestTemplate restTemplate;
 
   public String getCompanyVehicleLicensePlate(int companyId, int vehicleId) {
-    String url = String.format("%s/%d/vehicles/%d", PORTAL_PATH, companyId, vehicleId);
+    String url = String.format("%s/%d/vehicles/%d/license-plate", PORTAL_PATH, companyId, vehicleId);
     return restTemplate.getForObject(url, String.class);
+  }
+
+  public String getCompanyDriverFullName(int companyId, String driverId) {
+    String url = String.format("%s/%d/drivers/%s/full-name", PORTAL_PATH, companyId, driverId);
+    return restTemplate.getForObject(url, String.class);
+  }
+
+  public boolean validateDriverInCompany(int companyId, String driverId) {
+    String url = String.format("%s/%d/drivers/%s/validate", PORTAL_PATH, companyId, driverId);
+    return Boolean.TRUE.equals(restTemplate.getForObject(url, Boolean.class));
   }
 }
