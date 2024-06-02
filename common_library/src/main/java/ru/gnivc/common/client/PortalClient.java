2fc08ea46dcdffc570d965f6cdfc9cf8a21c5388
ath31st <ath31st@yandex.ru>
2024-06-02 14:36:24 +0300

    common || rework PortalClient

--- common_library/src/main/java/ru/gnivc/common/client/PortalClient.java
+++ common_library/src/main/java/ru/gnivc/common/client/PortalClient.java
@@ -5,21 +5,24 @@ import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 public class PortalClient {
-  private final String PORTAL_PATH = "http://portal/portal/v1/companies";
+  private final String portalUrl;
   private final RestTemplate restTemplate;
 
   public String getCompanyVehicleLicensePlate(int companyId, int vehicleId) {
-    String url = String.format("%s/%d/vehicles/%d/license-plate", PORTAL_PATH, companyId, vehicleId);
+    String url = String.format("%s/companies/%d/vehicles/%d/license-plate",
+        portalUrl, companyId, vehicleId);
     return restTemplate.getForObject(url, String.class);
   }
 
   public String getCompanyDriverFullName(int companyId, String driverId) {
-    String url = String.format("%s/%d/drivers/%s/full-name", PORTAL_PATH, companyId, driverId);
+    String url = String.format("%s/companies/%d/drivers/%s/full-name",
+        portalUrl, companyId, driverId);
     return restTemplate.getForObject(url, String.class);
   }
 
   public boolean validateDriverInCompany(int companyId, String driverId) {
-    String url = String.format("%s/%d/drivers/%s/validate", PORTAL_PATH, companyId, driverId);
+    String url = String.format("%s/companies/%d/drivers/%s/validate",
+        portalUrl, companyId, driverId);
     return Boolean.TRUE.equals(restTemplate.getForObject(url, Boolean.class));
   }
 }
