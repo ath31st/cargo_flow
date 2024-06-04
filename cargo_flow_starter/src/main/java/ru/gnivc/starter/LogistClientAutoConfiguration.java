c2bd0fc9ab6dad103735c47b8288a1173391cfab
ath31st <ath31st@yandex.ru>
2024-06-04 15:19:59 +0300

    common || add LogistClient autoconfig

---
+++ cargo_flow_starter/src/main/java/ru/gnivc/starter/LogistClientAutoConfiguration.java
@@ -0,0 +1,31 @@
+package ru.gnivc.starter;
+
+import java.util.Collections;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import ru.gnivc.common.client.LogistClient;
+import ru.gnivc.common.interceptor.TokenInterceptor;
+import ru.gnivc.starter.config.StarterProperties;
+
+@Configuration
+@ConditionalOnClass(LogistClient.class)
+@EnableConfigurationProperties(StarterProperties.class)
+public class LogistClientAutoConfiguration {
+
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.setInterceptors(Collections.singletonList(new TokenInterceptor()));
+    return restTemplate;
+  }
+
+  @Bean
+  public LogistClient logistClient(StarterProperties props, RestTemplate restTemplate) {
+    return new LogistClient(props.getPortalUrl(), restTemplate);
+  }
+}
--- cargo_flow_starter/src/main/java/ru/gnivc/starter/config/StarterProperties.java
+++ cargo_flow_starter/src/main/java/ru/gnivc/starter/config/StarterProperties.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "cargo-flow-starter")
 public class StarterProperties {
   private String portalUrl = "default_url";
+  private String logistUrl = "default_url";
 
   public void setPortalUrl(String portalUrl) {
     this.portalUrl = portalUrl;
@@ -12,5 +13,13 @@ public class StarterProperties {
 
   public String getPortalUrl() {
     return portalUrl;
+  }
+
+  public void setLogistUrl(String logistUrl) {
+    this.logistUrl = logistUrl;
+  }
+
+  public String getLogistUrl() {
+    return this.logistUrl;
   }
 }
