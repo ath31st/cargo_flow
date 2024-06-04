cbb1eb9391df4abb7bd9f60064ee30920dfd2ac0
ath31st <ath31st@yandex.ru>
2024-06-04 15:20:25 +0300

    driver || add SecurityConfig and TaskController

---
+++ driver_service/src/main/java/ru/gnivc/driver/config/TaskController.java
@@ -0,0 +1,35 @@
+package ru.gnivc.driver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gnivc.common.client.LogistClient;
+import ru.gnivc.common.dto.TaskDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/driver/v1/companies/{companyId}/tasks")
+public class TaskController {
+  private final LogistClient logistClient;
+
+  @PreAuthorize("@permissionValidator.hasCompanyDriverAccess(#companyId.toString())")
+  @GetMapping("/{taskId}")
+  public ResponseEntity<TaskDto> getTask(@PathVariable Integer companyId,
+                                         @PathVariable Integer taskId) {
+    return ResponseEntity.ok().body(logistClient.getTaskDtoById(companyId, taskId));
+  }
+
+//  @PreAuthorize("@permissionValidator.hasCompanyLogistAccess(#companyId.toString())")
+//  @GetMapping("/all")
+//  public ResponseEntity<Page<TaskDto>> allCompanyTask(
+//      @RequestParam(defaultValue = "0") Integer pageNumber,
+//      @RequestParam(defaultValue = "10") Integer pageSize,
+//      @PathVariable Integer companyId) {
+//    Pageable pageable = PageRequest.of(pageNumber, pageSize);
+//    return ResponseEntity.ok().body(taskService.getPageTask(companyId, pageable));
+//  }
+}
