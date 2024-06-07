a6e058ab6eb7e8ee9a9999f067df3897866b77f9
ath31st <ath31st@yandex.ru>
2024-06-07 11:07:24 +0300

    common || add method for getting all routes from logist service

--- common_library/src/main/java/ru/gnivc/common/client/LogistClient.java
+++ common_library/src/main/java/ru/gnivc/common/client/LogistClient.java
@@ -1,10 +1,17 @@
 package ru.gnivc.common.client;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import ru.gnivc.common.dto.NewTaskRouteReq;
+import ru.gnivc.common.dto.RestResponsePage;
 import ru.gnivc.common.dto.TaskDto;
+import ru.gnivc.common.dto.TaskRouteDto;
 
 @RequiredArgsConstructor
 public class LogistClient {
@@ -20,5 +27,20 @@ public class LogistClient {
     String url = String.format("%s/companies/%d/tasks/%d/routes/create-route",
         logistUrl, companyId, taskId);
     return restTemplate.postForObject(url, req, HttpStatus.class);
+  }
+
+  public Page<TaskRouteDto> getPageTaskRoute(int companyId, int taskId, Pageable pageable) {
+    String url = String.format("%s/companies/%d/tasks/%d/routes/all?pageNumber=%d&pageSize=%d",
+        logistUrl, companyId, taskId, pageable.getPageNumber(), pageable.getPageSize());
+
+    ResponseEntity<RestResponsePage<TaskRouteDto>> response = restTemplate.exchange(
+        url,
+        HttpMethod.GET,
+        null,
+        new ParameterizedTypeReference<>() {
+        }
+    );
+
+    return response.getBody();
   }
 }
