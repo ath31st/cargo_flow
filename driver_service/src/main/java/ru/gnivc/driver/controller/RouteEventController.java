dcf85159c9043446fa7d3adefca46d351c78aead
ath31st <ath31st@yandex.ru>
2024-06-07 11:26:29 +0300

    driver || add endpoint for getting all events by route for a driver

--- driver_service/src/main/java/ru/gnivc/driver/controller/RouteEventController.java
+++ driver_service/src/main/java/ru/gnivc/driver/controller/RouteEventController.java
@@ -1,14 +1,20 @@
 package ru.gnivc.driver.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.gnivc.common.dto.RouteEventShortDto;
 import ru.gnivc.driver.service.RouteEventService;
 
 @RestController
@@ -28,14 +34,16 @@ public class RouteEventController {
     return ResponseEntity.ok(HttpStatus.CREATED);
   }
 
-//  @PreAuthorize("@permissionValidator.hasCompanyLogistAccess(#companyId.toString())")
-//  @GetMapping("/all")
-//  public ResponseEntity<Page<TaskRouteDto>> allTaskRoutes(
-//      @RequestParam(defaultValue = "0") Integer pageNumber,
-//      @RequestParam(defaultValue = "10") Integer pageSize,
-//      @PathVariable Integer companyId,
-//      @PathVariable Integer taskId) {
-//    Pageable pageable = PageRequest.of(pageNumber, pageSize);
-//    return ResponseEntity.ok().body(taskRouteService.getPageTaskRoute(companyId, taskId, pageable));
-//  }
+  @PreAuthorize("@permissionValidator.hasCompanyDriverAccess(#companyId.toString())")
+  @GetMapping("/all-events")
+  public ResponseEntity<Page<RouteEventShortDto>> allRouteEvents(
+      @RequestParam(defaultValue = "0") Integer pageNumber,
+      @RequestParam(defaultValue = "10") Integer pageSize,
+      @PathVariable Integer companyId,
+      @PathVariable Integer taskId,
+      @PathVariable Integer routeId) {
+    Pageable pageable = PageRequest.of(pageNumber, pageSize);
+    return ResponseEntity.ok().body(
+        routeEventService.getPageRouteEvents(companyId, taskId, routeId, pageable));
+  }
 }
--- driver_service/src/main/java/ru/gnivc/driver/service/RouteEventService.java
+++ driver_service/src/main/java/ru/gnivc/driver/service/RouteEventService.java
@@ -2,9 +2,13 @@ package ru.gnivc.driver.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import ru.gnivc.common.client.LogistClient;
 import ru.gnivc.common.dto.RouteEventDto;
+import ru.gnivc.common.dto.RouteEventShortDto;
 import ru.gnivc.common.exception.RouteEventServiceException;
 import ru.gnivc.common.validator.EventTypeValidator;
 import ru.gnivc.common.wrapper.RouteEventWrapper;
@@ -13,6 +17,7 @@ import ru.gnivc.driver.service.producer.RouteEventProducer;
 @Service
 @RequiredArgsConstructor
 public class RouteEventService {
+  private final LogistClient logistClient;
   private final RouteEventProducer routeEventProducer;
 
   public void createRouteEvent(Integer companyId, Integer taskId,
@@ -27,5 +32,10 @@ public class RouteEventService {
     } catch (JsonProcessingException e) {
       throw new RouteEventServiceException(HttpStatus.BAD_REQUEST, e.getMessage());
     }
+  }
+
+  public Page<RouteEventShortDto> getPageRouteEvents(Integer companyId, Integer taskId,
+                                                     Integer routeId, Pageable pageable) {
+    return logistClient.getPageRouteEvents(companyId, taskId, routeId, pageable);
   }
 }
