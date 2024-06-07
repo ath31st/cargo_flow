cc76dbc118f4f287711aad74be1081254a7b3881
ath31st <ath31st@yandex.ru>
2024-06-07 12:30:26 +0300

    logist || driver || add methods in services for getting all route locations

--- driver_service/src/main/java/ru/gnivc/driver/service/RouteLocationService.java
+++ driver_service/src/main/java/ru/gnivc/driver/service/RouteLocationService.java
@@ -2,17 +2,22 @@ package ru.gnivc.driver.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import ru.gnivc.driver.dto.NewRouteLocationReq;
+import ru.gnivc.common.client.LogistClient;
 import ru.gnivc.common.dto.RouteLocationDto;
+import ru.gnivc.common.dto.RouteLocationShortDto;
 import ru.gnivc.common.exception.RouteEventServiceException;
 import ru.gnivc.common.wrapper.RouteLocationWrapper;
+import ru.gnivc.driver.dto.NewRouteLocationReq;
 import ru.gnivc.driver.service.producer.RouteLocationProducer;
 
 @Service
 @RequiredArgsConstructor
 public class RouteLocationService {
+  private final LogistClient logistClient;
   private final RouteLocationProducer routeLocationProducer;
 
   public void createRouteLocation(Integer companyId, Integer taskId,
@@ -26,5 +31,10 @@ public class RouteLocationService {
     } catch (JsonProcessingException e) {
       throw new RouteEventServiceException(HttpStatus.BAD_REQUEST, e.getMessage());
     }
+  }
+
+  public Page<RouteLocationShortDto> getPageRouteLocations(Integer companyId, Integer taskId,
+                                                           Integer routeId, Pageable pageable) {
+    return logistClient.getPageRouteLocations(companyId, taskId, routeId, pageable);
   }
 }
--- logist_service/src/main/java/ru/gnivc/logist/service/RouteLocationService.java
+++ logist_service/src/main/java/ru/gnivc/logist/service/RouteLocationService.java
@@ -1,13 +1,19 @@
 package ru.gnivc.logist.service;
 
 import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.gnivc.common.dto.RouteLocationDto;
+import ru.gnivc.common.dto.RouteLocationShortDto;
 import ru.gnivc.common.wrapper.RouteLocationWrapper;
 import ru.gnivc.logist.entity.RouteLocation;
 import ru.gnivc.logist.entity.TaskRoute;
+import ru.gnivc.logist.mapper.RouteLocationMapper;
 import ru.gnivc.logist.repository.RouteLocationRepository;
 
 @Service
@@ -15,6 +21,7 @@ import ru.gnivc.logist.repository.RouteLocationRepository;
 public class RouteLocationService {
   private final RouteLocationRepository routeLocationRepository;
   private final TaskRouteService taskRouteService;
+  private final RouteLocationMapper routeLocationMapper;
 
   @Transactional
   public void createRouteLocation(RouteLocationWrapper wrapper) {
@@ -36,5 +43,16 @@ public class RouteLocationService {
     rl.setRecordedAt(Instant.now());
 
     routeLocationRepository.save(rl);
+  }
+
+  public Page<RouteLocationShortDto> getPageRouteLocations(Integer companyId, Integer taskId,
+                                                           Integer routeId, Pageable pageable) {
+    Page<RouteLocation> locations =
+        routeLocationRepository.findPageRouteLocations(companyId, taskId, routeId, pageable);
+    List<RouteLocationShortDto> dtoList = locations.stream()
+        .map(routeLocationMapper::toShortDto)
+        .toList();
+
+    return new PageImpl<>(dtoList, pageable, locations.getTotalElements());
   }
 }
