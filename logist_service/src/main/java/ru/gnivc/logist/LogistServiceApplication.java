57bddbc6b19ff346c8591b75172332ddf7e4883c
ath31st <ath31st@yandex.ru>
2024-06-08 15:26:36 +0300

    logist || add scheduler config and annotation

--- logist_service/src/main/java/ru/gnivc/logist/LogistServiceApplication.java
+++ logist_service/src/main/java/ru/gnivc/logist/LogistServiceApplication.java
@@ -2,7 +2,9 @@ package ru.gnivc.logist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class LogistServiceApplication {
 
--- logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
+++ logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
@@ -1,11 +1,14 @@
 package ru.gnivc.logist.config;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import ru.gnivc.common.client.PortalClient;
 import ru.gnivc.logist.service.StatisticsService;
 import ru.gnivc.logist.service.producer.StatisticsProducer;
 
@@ -17,9 +20,15 @@ import ru.gnivc.logist.service.producer.StatisticsProducer;
 public class SchedulerConfig {
   private final StatisticsProducer statisticsProducer;
   private final StatisticsService statisticsService;
+  private final PortalClient portalClient;
 
   @Scheduled(fixedRate = 10000, initialDelay = 5000)
   private void test() {
-    log.info("test scheduler");
+    try {
+      final List<Integer> companyIds = portalClient.getCompanyIds();
+      log.info(companyIds.stream().map(Object::toString).collect(Collectors.joining()));
+    } catch (Exception e) {
+      log.error(e.getMessage());
+    }
   }
 }
