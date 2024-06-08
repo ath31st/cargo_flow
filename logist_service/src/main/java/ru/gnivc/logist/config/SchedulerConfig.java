ee41f9daf58fadd4c5e678880b68c66cf02e21c7
ath31st <ath31st@yandex.ru>
2024-06-08 12:26:19 +0300

    logist || add StatisticsProducer and config for a topic

--- logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
+++ logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
@@ -1,16 +1,25 @@
 package ru.gnivc.logist.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import ru.gnivc.logist.service.StatisticsService;
+import ru.gnivc.logist.service.producer.StatisticsProducer;
 
+@Slf4j
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 @RequiredArgsConstructor
 public class SchedulerConfig {
+  private final StatisticsProducer statisticsProducer;
   private final StatisticsService statisticsService;
 
+  @Scheduled(fixedRate = 10000, initialDelay = 5000)
+  private void test() {
+    log.info("test scheduler");
+  }
 }
---
+++ logist_service/src/main/java/ru/gnivc/logist/service/producer/StatisticsProducer.java
@@ -0,0 +1,42 @@
+package ru.gnivc.logist.service.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import ru.gnivc.common.wrapper.StatisticsWrapper;
+import ru.gnivc.logist.service.StatisticsService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StatisticsProducer {
+  private final StatisticsService statisticsService;
+
+  @Value("${kafka.topic.company_statistics.name}")
+  private String companyStatisticsTopic;
+
+  private final ObjectMapper objectMapper;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  public void sendMessage(StatisticsWrapper wrapper) throws JsonProcessingException {
+    String statisticsAsMessage = objectMapper.writeValueAsString(wrapper);
+
+    CompletableFuture<SendResult<String, String>> future
+        = kafkaTemplate.send(companyStatisticsTopic, statisticsAsMessage);
+
+    future.whenComplete((result, ex) -> {
+      if (ex == null) {
+        log.info("Sent message=[{}] with offset=[{}]", statisticsAsMessage,
+            result.getRecordMetadata().offset());
+      } else {
+        log.error("Unable to send message=[{}] due to : {}", statisticsAsMessage, ex.getMessage());
+      }
+    });
+  }
+}
