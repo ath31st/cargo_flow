85ef6b70cb1af02d471b4c828712b7f3e0f918ef
ath31st <ath31st@yandex.ru>
2024-06-08 19:23:14 +0300

    logist || update SchedulerConfig

--- logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
+++ logist_service/src/main/java/ru/gnivc/logist/config/SchedulerConfig.java
@@ -1,7 +1,7 @@
 package ru.gnivc.logist.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import ru.gnivc.common.client.PortalClient;
+import ru.gnivc.common.wrapper.StatisticsWrapper;
+import ru.gnivc.logist.dto.CompanyStatisticsDailyDto;
 import ru.gnivc.logist.service.StatisticsService;
 import ru.gnivc.logist.service.producer.StatisticsProducer;
 
@@ -21,12 +23,19 @@ public class SchedulerConfig {
   private final StatisticsProducer statisticsProducer;
   private final StatisticsService statisticsService;
   private final PortalClient portalClient;
+  private final ObjectMapper objectMapper;
 
   @Scheduled(fixedRate = 10000, initialDelay = 5000)
   private void test() {
     try {
-      final List<Integer> companyIds = portalClient.getCompanyIds();
-      log.info(companyIds.stream().map(Object::toString).collect(Collectors.joining()));
+      List<Integer> companyIds = portalClient.getCompanyIds();
+      if (!companyIds.isEmpty()) {
+        for (Integer companyId : companyIds) {
+          CompanyStatisticsDailyDto dto = statisticsService.getDailyStatistics(companyId);
+          statisticsProducer.sendMessage(
+              new StatisticsWrapper(companyId, objectMapper.writeValueAsString(dto)));
+        }
+      }
     } catch (Exception e) {
       log.error(e.getMessage());
     }
